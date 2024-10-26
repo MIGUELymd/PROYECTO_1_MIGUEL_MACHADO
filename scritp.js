ab7d159fdfa3c4d98e7f779b0a202048621a9135
Miguel <yonaikel.diaz27@gmail.com>
2024-10-26 11:34:33 -0400

    Commit Frontend

--- scritp.js
+++ scritp.js
@@ -1,1 +1,13 @@
 console.log("hola_gente");
+function imprimirnum (){
+let resultado = "";
+for (let i = 1; i <= 20; i++) {
+    if( i % 2 === 0){
+        resultado += 1 + " es par<br>";
+    }
+    else{
+        resultado += i + "es impar<br>";
+    }
+}
+document.getElementById("resultado").innerHTML = resultado;
+};
